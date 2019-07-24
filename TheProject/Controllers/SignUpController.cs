fa2f6b9032f62e37090c864db4c59f17ce5369f5
TimothyMcWatters <timmch2o@gmail.com>
2019-07-24 13:06:17 -0500

    signup controller added

---
+++ TheProject/Controllers/SignUpController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace TheProject.Controllers
+{
+    public class SignUpController : Controller
+    {
+
+        public void PostUserData(string firstName, string lastName, string userEmail, string userPassword)
+        {
+
+
+
+        }
+
+        public class SignUpScreen
+        {
+            public string UserEmail { get; set; }
+            public string UserPassword { get; set; }
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
