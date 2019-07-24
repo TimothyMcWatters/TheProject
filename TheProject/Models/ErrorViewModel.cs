ff449386627feefbc745dfa7696e015abbf2dcf5
TimothyMcWatters <timmch2o@gmail.com>
2019-07-24 05:02:18 -0500

    Add project files.

---
+++ TheProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
